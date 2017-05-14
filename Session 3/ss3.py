032e86fa91d5b4fd70498e9e059ff67156aa0d25
DESKTOP-EN6NUV2\Admin <manhpt1598@gmail.com>
2017-05-14 12:26:19 +0700

    no message

--- Session 3/ss3.py
+++ Session 3/ss3.py
@@ -1,19 +1,31 @@
-cuahang = ["T-Shirt","Sweater"]
-print("Welcome t’o our shop, what do you want (C, R, U, D)? C")
-item = input("Enter new item :")
-cuahang.append(item)
-print("Our items :",cuahang)
-print("")
-print("Welcome to our shop, what do you want (C, R, U, D)? R")
-print("Our items :",cuahang)
-print("")
-print("Welcome to our shop, what do you want (C, R, U, D)? U")
-print("Update position? 1")
-item2 = input("Enter new item :")
-cuahang[1] = item2
-print("Our items:",cuahang)
-print("")
-print("Welcome to our shop, what do you want (C, R, U, D)? D")
-item3 = input("Delete item? :")
-cuahang.remove(item3)
-print("Our items:",cuahang)
+cuahang = ["T-Shirt", "Sweater", "Jeans"]
+
+print("===========================================")
+print(" Add = A, Update = U, Delete = D")
+print("===========================================")
+
+while True:
+
+    print("")
+    action = input("Welcome to our shop, what do you want ( A, U, D) ?")
+    print(action)
+    action = action.upper()
+
+    print("Items is shop:")
+    item_no = 1
+    for items in cuahang:
+        print("{0}.{1}".format(item_no,items))
+        item_no +=1
+
+
+    if action =="A":
+        item = input("Add new item: ")
+        cuahang.append(item)
+    elif action =="U":
+        item_no = int(input("Update new item:"))-1
+        new_item = input("New item:")
+        cuahang.insert(ite_no, item_new)
+
+    elif action =="D":
+        delete_item = input("Delete item")
+        cuahang.remove(delete_item)
